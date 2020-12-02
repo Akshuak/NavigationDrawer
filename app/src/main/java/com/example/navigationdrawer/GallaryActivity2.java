e1969290ff7fbfd1d039d61725133fec67aea8d1
Akshuak <akshuakkiyalbek@gmail.com>
2020-12-02 22:40:58 +0600

    'workinggg'

---
+++ app/src/main/java/com/example/navigationdrawer/GallaryActivity2.java
@@ -0,0 +1,15 @@
+package com.example.navigationdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GallaryActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallary2);
+        getSupportActionBar().setTitle(("Gallarey"));
+    }
+}
